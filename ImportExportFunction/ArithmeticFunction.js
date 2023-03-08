eaae4bcc4b4e913c48810e0b2bfe5862c858695d
Mohammad Shaikh <94097592+shaikhmd007@users.noreply.github.com>
2023-03-08 21:34:13 +0530

    Arithmetic Operation

---
+++ ImportExportFunction/ArithmeticFunction.js
@@ -0,0 +1,23 @@
+const Addition = (n1, n2) => {
+    return n1 + n2;
+}
+
+const Substraction = (n1, n2) => {
+    return n1 - n2;
+}
+
+const Division = (n1, n2) => {
+    return n1 / n2;
+}
+
+const Multiplication = (n1, n2) => {
+    return n1 * n2;
+}
+
+
+module.exports = {
+    Addition: Addition,
+    Substraction: Substraction,
+    Division: Division,
+    Multiplication: Multiplication,
+};
---
+++ ImportExportFunction/index.js
@@ -0,0 +1,10 @@
+const ArithmaticOperations = require("./ArithmeticFunction");
+console.log(ArithmaticOperations);
+
+// Calling Function
+
+console.log(`Addition is `, ArithmaticOperations.Addition(10, 5));
+console.log(ArithmaticOperations.Substraction(10, 5));
+console.log(ArithmaticOperations.Multiplication(10, 5));
+console.log('Division is', ArithmaticOperations.Division(20, 5));
+
