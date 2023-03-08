3e22a0736fb6c9578750dbf95d9bedd7c8bbb0e8
Mohammad Shaikh <94097592+shaikhmd007@users.noreply.github.com>
2023-03-08 21:20:21 +0530

    Update index.js

--- CommonJS vs ES6 Modules/index.js
+++ CommonJS vs ES6 Modules/index.js
@@ -1,11 +1,10 @@
 
 //This is CommonJS Practices to require the module
-/* const requireCommonJS = require('./CommonJSModule')
-console.log(requireCommonJS); */
+ const requireCommonJS = require('./CommonJSModule')
+console.log(requireCommonJS); 
 
 
 //This is ES6 Practices to import the module
-
-// console.log('-->> This is ES6 Practices to require the module');
 import ES6Module from './ES6Module';
+console.log('-->> This is ES6 Practices to require the module');
 console.log(ES6Module);
